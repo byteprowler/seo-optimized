bc406c134ad2ee82b193bcba38fd286cdefd0f0b
Ogojoshua905 <joshuaexcellency1@gmail.com>
2025-04-20 19:56:23 +0100

    feat: Add next-sitemap and next-seo configuration files, update package dependencies

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import { DefaultSeo } from "next-seo";
-import SEO from "@/next-seo.config";
+import SEO from "../../next-seo.config";
 import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
